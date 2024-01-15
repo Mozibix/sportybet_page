ceb3f6af53b0e804b6b5561fe9596d8f3c70bbe7
Mozibix <opuzmoses@gmail.com>
2024-01-15 12:30:55 +0100

    feats: added animated features

--- script.js
+++ script.js
@@ -8,5 +8,62 @@ document.addEventListener("DOMContentLoaded", () => {
 
   setTimeout(() => {
     preloader.classList.add("hidden");
-  }, 2000);
+  }, 500);
 });
+
+/* SCROLL FUNCTIONALITY */
+const animationUp = document.querySelectorAll(".animations_up");
+const animationDown = document.querySelectorAll(".animations_down");
+const animationLeft = document.querySelectorAll(".animations_left");
+const animationRight = document.querySelectorAll(".animations_right");
+const animationSpecial = document.querySelectorAll(".animations_left_special");
+const animation = document.querySelectorAll(".animations");
+
+const observer = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("scroll-animation");
+      } else {
+        entry.target.classList.remove("scroll-animation");
+      }
+    });
+  },
+  { threshold: 0.1 }
+);
+
+for (let i = 0; i < animationUp.length; i++) {
+  const elements = animationUp[i];
+
+  observer.observe(elements);
+}
+/*  */
+for (let i = 0; i < animationDown.length; i++) {
+  const elements = animationDown[i];
+
+  observer.observe(elements);
+}
+/*  */
+for (let i = 0; i < animationLeft.length; i++) {
+  const elements = animationLeft[i];
+
+  observer.observe(elements);
+}
+/*  */
+for (let i = 0; i < animationRight.length; i++) {
+  const elements = animationRight[i];
+
+  observer.observe(elements);
+}
+/*  */
+for (let i = 0; i < animationSpecial.length; i++) {
+  const elements = animationSpecial[i];
+
+  observer.observe(elements);
+}
+/*  */
+for (let i = 0; i < animation.length; i++) {
+  const elements = animation[i];
+
+  observer.observe(elements);
+}
